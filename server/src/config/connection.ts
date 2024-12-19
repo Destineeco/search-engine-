a7d4bc987d06489e1bfca52c5d13034a397d9010
Destinee  <xdexstxinxeex@gmail.com>
2024-12-18 21:18:40 -0600

    updated connection.ts file

--- server/src/config/connection.ts
+++ server/src/config/connection.ts
@@ -2,4 +2,7 @@ import mongoose from 'mongoose';
 
 mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://Destinee:<desto8834>@booksearchproject.urq4s.mongodb.net/?retryWrites=true&w=majority&appName=BookSearchProject');
 
-export default mongoose.connection;
+
+const db = mongoose.connection;
+
+export default db;
