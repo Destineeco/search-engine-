6245436abf8e5546a30a419b51fb0ba990c30ca5
Destinee  <xdexstxinxeex@gmail.com>
2024-12-18 21:06:31 -0600

    updated for mongo atlas

--- server/src/config/connection.ts
+++ server/src/config/connection.ts
@@ -1,5 +1,5 @@
 import mongoose from 'mongoose';
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/googleBooks');
+mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://Destinee:<desto8834>@booksearchproject.urq4s.mongodb.net/?retryWrites=true&w=majority&appName=BookSearchProject');
 
 export default mongoose.connection;
